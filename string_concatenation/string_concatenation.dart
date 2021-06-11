687033a99d125016ed71f572bbb8b11a818c940e
Matheus Gobetti <matheusgobetti12@gmail.com>
2021-06-10 23:14:28 -0300

    Concatenação de Strings

---
+++ string_concatenation/string_concatenation.dart
@@ -0,0 +1,17 @@
+main() {
+  //Simple concatenation
+  var olaMundo = "Hello" + "world";
+  print(olaMundo);
+
+  var nome = "Matheus";
+  var sobrenome = "Gobetti";
+
+  var nomeCompleto = nome + sobrenome;
+  print(nomeCompleto);
+
+  //Alternative concatenation
+  var name = "Matheus";
+  var lastName = "Gobetti";
+  var fullName = "$name $lastName";
+  print(fullName);
+}
