476d26a1e00e990e4e9e9ebf9a699ea20e206391
Matheus Gobetti <matheusgobetti12@gmail.com>
2021-06-18 13:50:01 -0300

    Typing lists

---
+++ arrays/typing_arrays.dart
@@ -0,0 +1,19 @@
+main() {
+  //Tipando um array de Strings
+  List<String> nomes = ["Matheus", "Júlia", "Letícia", "Beatriz"];
+
+  nomes.add("Carolina"); // Adiciona
+
+  print(nomes);
+  print(nomes.length); // Tamanho do array
+  print(nomes[0]);
+  print(nomes[1]);
+
+  nomes.remove("Beatriz"); // Remove por nome
+
+  print(nomes);
+
+  nomes.removeAt(1); // Remove por posição
+
+  print(nomes);
+}
